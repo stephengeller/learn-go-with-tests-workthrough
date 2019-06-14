da0b4392bc5e0d23874a5315365b2f3b404a9c0c
Stephen Geller <stephen.geller+git@gmail.com>
2019-06-14 12:36:03 +0100

    Dependency Injection section done

---
+++ internal/greeter_test.go
@@ -0,0 +1,18 @@
+package internal
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreeter(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
